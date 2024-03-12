a972c1e35b30a009043f5cd471c7a55c9a766d41
Stephane Koman <stefchris2@gmaoil.com>
2024-03-12 22:48:31 +0100

    Fix Mapper issue

--- src/main/java/io/skoman/multitenant/mappers/TenantMapper.java
+++ src/main/java/io/skoman/multitenant/mappers/TenantMapper.java
@@ -4,12 +4,16 @@ import io.skoman.multitenant.dtos.TenantCreaDTO;
 import io.skoman.multitenant.dtos.TenantDTO;
 import io.skoman.multitenant.entities.Tenant;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface TenantMapper {
     TenantMapper INSTANCE = Mappers.getMapper(TenantMapper.class);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     Tenant tenantCreaDTOToTenant(TenantCreaDTO dto);
 
     TenantDTO tenantToTenantDTO(Tenant tenant);
--- src/main/java/io/skoman/multitenant/mappers/TodoMapper.java
+++ src/main/java/io/skoman/multitenant/mappers/TodoMapper.java
@@ -4,12 +4,17 @@ import io.skoman.multitenant.dtos.TodoCreaDTO;
 import io.skoman.multitenant.dtos.TodoDTO;
 import io.skoman.multitenant.entities.Todo;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface TodoMapper {
     TodoMapper INSTANCE = Mappers.getMapper(TodoMapper.class);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "tenant", ignore = true)
     Todo todoCreaDTOToTodo(TodoCreaDTO dto);
 
     TodoDTO todoToTodoDTO(Todo todo);
